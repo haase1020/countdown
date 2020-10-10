bcad865a8e84704c8e9e63e7e9b766b792907e00
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-10 13:35:55 -0400

    work on functionality for date picker

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
+
 // set date input minimum as today's date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// take values from form input
+function updateCountdown(e) {
+    e.preventDefault(e);
+    countdownTitle = e.srcElement[0].value
+    countdownDate = e.srcElement[1].value;
+
+    console.log(countdownTitle, countdownDate)
+}
+
+
+
+// event listeners
+countdownForm.addEventListener('submit', updateCountdown);
